5e53196689e7a0d14a6a0c111e486f55401ed658
joaozinsh <joaogabriel241201@gmail.com>
2021-05-31 11:09:33 -0300

    feature: task3 criação e conexão com o bd

---
+++ SpringBoot/saudedobem/saudedobem/src/test/java/org/generation/sb/saudedobem/SaudeDoBemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.sb.saudedobem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeDoBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
