841d589ed95c86b8d5b3fbc296da912c525e14c3
joaozinsh <joaogabriel241201@gmail.com>
2021-05-31 17:45:13 -0300

    feature: criação das models

---
+++ SpringBoot/saudedobem/saudedobem/src/main/java/org/generation/sb/saudedobem/model/Usuario.java
@@ -0,0 +1,102 @@
+package org.generation.sb.saudedobem.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(max = 255)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 5, max = 20)
+	private String usuario;
+	
+	@NotNull
+	@Size(min = 6, max = 100)
+	private String email;
+	
+	@NotNull
+	@Size(min = 5, max = 16)
+	private String senha;
+	
+	@NotNull
+	@Column(columnDefinition = "ENUM('Administrador','Cliente')")
+	private String tipo;
+
+	@OneToMany(mappedBy = "id_usuario")
+	private List<Venda> venda;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Venda> getVenda() {
+		return venda;
+	}
+
+	public void setVenda(List<Venda> venda) {
+		this.venda = venda;
+	}
+	
+}
