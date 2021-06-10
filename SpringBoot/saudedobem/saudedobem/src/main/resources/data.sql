760006bf2e4afdf4d16c5a61c9d871c8d4ccb59c
Gabriel Cerdeira <83592437+gabrgcr@users.noreply.github.com>
2021-06-10 10:59:46 -0300

    Fix: Updating data.sql

--- SpringBoot/saudedobem/saudedobem/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Diabetes", "evitar açúcar", 0);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Asma", "Inflamação das vias aéreas", 0);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Cancer", "Aumento das células cancerigenas", 0);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Depressão", "Constante sensação de tristeza, pessimismo e baixa autoestima.", 0);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Hipertenção Arterial", "Pressão Alta", 0);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Sarampo", "Doença infecciosa grave, causada por um vírus", 1);
-INSERT INTO tb_doenca (nome, descricao, transmissivel) VALUES ("Gripe", "A gripe é uma doença causada por um vírus do tipo Influenza,", 1);
